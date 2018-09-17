86913fe34401b89e6acde7cddab2a3240eefa903
Akhilesh Anand <39920462+akhileshanand@users.noreply.github.com>
2018-09-18 00:00:33 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/Forms';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import {MemberListComponent} from './member/member-list.component';
-import {MemberListService} from './member/member-list.service';
+import { MemberListComponent } from './member/member-list.component';
+import { CustomerService } from './customer/customer.serice';
+import { ReportComponent } from './report/report.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RouterModule, Routes } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MemberListComponent
+    MemberListComponent,
+    CustomerComponent,
+    ReportComponent
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'Report' , component:ReportComponent}, 
+      {path:'Voting' , component:MemberListComponent}   
+      
+    ])
   ],
-  providers: [MemberListService],
+
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
